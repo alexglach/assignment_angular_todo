1b17f7639f5d3e6685f912f23cc8f203901002c9
Dylan Lynch <lynchd2@tcnj.edu>
2016-09-26 14:44:25 -0400

    Transfer to services

--- app.js
+++ app.js
@@ -1,33 +1,25 @@
 var app = angular.module("toDoApp",[])
 
-app.controller("TodoCtrl", ["$scope", "$window",function($scope, $window) {
+app.controller("TodoCtrl", ["$scope", "$window", "Item", function($scope, $window, Item) {
  
-
-  $scope.items = [{ text: "Get groceries from the store",
-                dueDate: new Date(),
-                completed: false }]
+  $scope.items = function() {
+    console.log("DSADS")
+  }
 
   $scope.submitForm = function(){
     var newItem = {};
     newItem.text = $scope.formData.name;
     newItem.dueDate = $scope.formData.dueDate;
-    newItem.completed = false;
-    $scope.items.push(newItem);
+    Item.create(newItem)
     $scope.formData = {};
   };
 
   $scope.deleteItem = function(item){
-    var index = $scope.items.indexOf(item);
-    $scope.items.splice(index, 1);
+    Item.deleteItem(item)
   }
 
   $scope.clearCompleted = function(){
-    var toBeDeleted = $scope.items.filter(function(item){
-      return item.completed;
-    });
-    for (var i = 0; i < toBeDeleted.length; i++) {
-      $scope.deleteItem(toBeDeleted[i]);
-    }
+    Item.clearCompleted();
   }
 
   $scope.filterText = "Hide Completed"
---
+++ services/allitems.js
@@ -0,0 +1,29 @@
+app.factory("Item", [function() {
+
+  var _items = [{ text: "Get groceries from the store",
+                dueDate: new Date(),
+                completed: false }]
+  return {
+    allItems: function(){
+      return _items
+    },
+    create: function(item) {
+      item.completed = false;
+      _items.push(item);
+    },
+    deleteItem: function(item) {
+      var index = _items.indexOf(item);
+      _items.splice(index,1);
+    },
+    clearCompleted: function() {
+      var allItems = this.allItems()
+      var toBeDeleted = allItems.filter(function(item){
+        return item.completed;
+      });
+      for (var i = 0; i < toBeDeleted.length; i++) {
+        deleteItem(toBeDeleted[i]);
+      }
+    }
+  }
+
+}])
